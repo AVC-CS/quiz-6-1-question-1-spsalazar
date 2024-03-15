f89ca121a9bb1c8fb1d8c6e63ed38ddda3a40272
spsalazar <156946827+spsalazar@users.noreply.github.com>
2024-03-15 22:15:14 +0000

    Testing out getTwoValues functions

--- main.hpp
+++ main.hpp
@@ -1,7 +1,18 @@
+#include <iostream>
+using namespace std;
 
+void getTwoValues(int, int);
 //********************
 // Declare Function Prototypes
 //********************
+
+void getTwoValues(int begin, int end) {
+    cout << "Please enter beginning and end numbers: ";
+    cin >> begin >> end;
+    if (begin >= end) {
+        getTwoValues(begin, end);
+    }
+}
 
 // ******************************
 // Implement all your functions here
