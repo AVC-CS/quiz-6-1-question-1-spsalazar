2b57690c497a261aaf55275195cb3d734fb8c0f6
spsalazar <156946827+spsalazar@users.noreply.github.com>
2024-03-15 22:53:01 +0000

    testing getNextPrime

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main()
 	int prime1, prime2;
 
 	getTwoValues(begin, end);
-	// prime1 = getNextPrime(begin);
-	// cout << "The prime number after " << begin << " is " << prime1 << endl;
+	prime1 = getNextPrime(begin);
+	cout << "The prime number after " << begin << " is " << prime1 << endl;
 	// prime2 = getPrevPrime(end);
 	// cout << "The prime number before " << end << " is " << prime2 << endl;
 }
--- main.hpp
+++ main.hpp
@@ -1,19 +1,47 @@
 #include <iostream>
 using namespace std;
 
-void getTwoValues(int, int);
+void getTwoValues(int&, int&);
+int getNextPrime(int);
 //********************
 // Declare Function Prototypes
 //********************
 
-void getTwoValues(int begin, int end) {
+bool isPrime;
+int nextNum;
+
+void getTwoValues(int &begin, int &end) {
     cout << "Please enter beginning and end numbers: ";
     cin >> begin >> end;
     if (begin >= end) {
         getTwoValues(begin, end);
     }
 }
+int getNextPrime(int begin) {
+    nextNum = begin + 1;
+    isPrime = false;
+      while (!isPrime) {
+        isPrime = true;
+        for (int i = 2; i * i <= nextNum; i++) {
+            if (nextNum % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (!isPrime) {
+            nextNum++;
+        }
+      }
+        return nextNum;
+}
 
+// int getPrevPrime(int end) {
+//     isPrime = false;
+//     while (!isPrime) {
+//         for int i = 2; i * i <= 
+//     }
+// }
+//1 3
 // ******************************
 // Implement all your functions here
 // ******************************
